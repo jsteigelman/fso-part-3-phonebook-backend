1743cb62e6a5d321670ea400900bf7cfab88323d
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-18 16:40:35 -0400

    refactor

--- index.js
+++ index.js
@@ -58,15 +58,6 @@ app.get('/api/persons/:id', (request, response) => {
   Person.findById(Number(request.params.id))
     .then(person => response.json(person))
     .catch(error => console.log(response.status(404).end()))
-  // const id = Number(request.params.id)
-  // const person = persons.find((personElement) => {
-  //   return personElement.id === id
-  // })
-  // if (person) {
-  //   response.json(person)
-  // } else {
-  //   response.status(404).end()
-  // }
 })
 
 // create single person
@@ -110,12 +101,49 @@ app.post('/api/persons', (request, response) => {
 
 // app.use(morgan(':method :url :body'))
 
-// delete single person
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((person) => person.id !== id)
-  response.status(204).end()
+// update a person
+app.put('/api/persons/:id', (request, response, next) => {
+  const updatedData = request.body
+  
+  const updatedPerson = {
+    name: updatedData.name
+  }
+
+  Person.findByIdAndUpdate(request.params.id, updatedPerson, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
+
+// delete single person
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+// handle errors
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
