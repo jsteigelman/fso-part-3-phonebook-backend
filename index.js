67c848b96cb9af29228c66aac1b59c7f5953f39e
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-18 10:41:17 -0400

    set up env variables

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URL
+
+mongoose.connect(url)
+
+// define a mongoose model
+const PersonSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', PersonSchema)
+
+const addPerson = new Person({
+  name: process.argv[2],
+  number: process.argv[3]
+})
+
+// if person info not provided, list all current contacts
+if (process.argv.length <= 2) {
+    Person.find({}).then((result) => {
+      result.forEach((person) => {
+        console.log(`Person: ${person.name} ${person.number}`)
+      })
+      mongoose.connection.close()
+    })
+  } else {
+    addPerson.save().then(result => {
+        console.log(`Added ${process.argv[2]} number ${process.argv[3]} to phonebook`)
+        mongoose.connection.close()
+      })
+  }
