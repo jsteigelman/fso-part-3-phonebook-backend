deb87e2e6a53861b07011ed0d00b1e5678b11b8b
Joey Steigelman <joey.steigelman@gmail.com>
2022-04-21 21:17:04 -0400

    refactor

--- mongo.js
+++ mongo.js
@@ -12,23 +12,3 @@ const PersonSchema = new mongoose.Schema({
 })
 
 const Person = mongoose.model('Person', PersonSchema)
-
-// const addPerson = new Person({
-//   name: process.argv[2],
-//   number: process.argv[3]
-// })
-
-// if person info not provided, list all current contacts
-// if (process.argv.length <= 2) {
-//     Person.find({}).then((result) => {
-//       result.forEach((person) => {
-//         console.log(`Person: ${person.name} ${person.number}`)
-//       })
-//       mongoose.connection.close()
-//     })
-//   } else {
-//     addPerson.save().then(result => {
-//         console.log(`Added ${process.argv[2]} number ${process.argv[3]} to phonebook`)
-//         mongoose.connection.close()
-//       })
-//   }
